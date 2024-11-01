bd64281e71656401aa2946e22a81ee58c1711bb5
人間工作 <coolxiaom95@gmail.com>
2024-11-02 00:45:44 +0800

    sealed

--- src/main/kotlin/MetaNoCommand.kt
+++ src/main/kotlin/MetaNoCommand.kt
@@ -2,10 +2,11 @@ package top.mrxiaom.commandyouwant
 
 import net.mamoe.mirai.message.data.*
 
-object MetaNoCommand : Message, MessageMetadata, ConstrainSingle {
+sealed class MetaNoCommand : Message, MessageMetadata, ConstrainSingle {
     override val key: MessageKey<MetaNoCommand> = Key
     object Key : MessageKey<MetaNoCommand> {
         override val safeCast: (SingleMessage) -> MetaNoCommand? = { it as? MetaNoCommand }
     }
     override fun toString(): String = contentToString()
+    companion object : MetaNoCommand()
 }
